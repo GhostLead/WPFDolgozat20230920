8cab643a54baef046b170419d01bb767de085f31
gcsanad <71721197+gcsanad@users.noreply.github.com>
2023-09-20 09:35:31 +0200

    Add files via upload

---
+++ WPFDoga2023_09_20/Fuvar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDoga2023_09_20
+{
+    public class Fuvar
+    {
+        int taxiAzon;
+        DateTime indulasIdopontja;
+        int utazasIdotartama;
+        int megtettTavolsag;
+        int vilteldid;
+        int borravalo;
+        string fizetesModja;
+
+        public Fuvar(string sor)
+        {
+            //string[] felbontas = sor.Split(';');
+            //this.taxiAzon = int.Parse(felbontas[0]);
+            //this.indulasIdopontja = DateTime.Parse(felbontas[1]);
+            //this.utazasIdotartama = int.Parse(felbontas[2]);
+            //this.megtettTavolsag = int.Parse(felbontas[3]);
+            //this.vilteldid = int.Parse(felbontas[4]);
+            //this.borravalo = int.Parse(felbontas[5]);
+            //this.fizetesModja = felbontas[6];
+        }
+
+        public int TaxiAzon { get => taxiAzon; }
+        public DateTime IndulasIdopontja { get => indulasIdopontja;}
+        public int UtazasIdotartama { get => utazasIdotartama; }
+        public int MegtettTavolsag { get => megtettTavolsag; }
+        public int Vilteldid { get => vilteldid; }
+        public int Borravalo { get => borravalo; }
+        public string FizetesModja { get => fizetesModja; }
+    }
+}
---
+++ WPFDoga2023_09_20/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFDoga2023_09_20
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// 
+    /// </summary>
+    //balra: Barizs Márton Dániel
+    //jobbra: -
+    public partial class MainWindow : Window
+    {
+        List<Fuvar> fuvarok = File.ReadAllLines("fuvar.csv").Skip(1).Select(x => new Fuvar(x)).ToList();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHarmadikFeladat_Click(object sender, RoutedEventArgs e)
+        {
+            int fuvarokSzama = fuvarok.Count();
+            MessageBox.Show($"3.Feladat: {fuvarokSzama} fuvar");
+        }
+
+        private void txtAzonositoEllenorzes(object sender, TextCompositionEventArgs e) {
+
+            Regex csakSzamok = new Regex("[^0-9]+");
+            e.Handled = csakSzamok.IsMatch(e.Text);
+        }
+        private void btnNegyedikFeladat_Click(object sender, RoutedEventArgs e)
+        {
+            int taxiAzon = int.Parse(txtTaxisAzonosito.Text);
+            if (fuvarok.Any(x => x.TaxiAzon == taxiAzon) == true)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Nincsen ilyen azonosítójú taxis");
+            }
+
+        }
+    }
+}
